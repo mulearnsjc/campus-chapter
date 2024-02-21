50f83088f513f99fd7a18941c7c7e90bbed9ab87
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-02-21 20:03:22 +0530

    Add Gatsby plugin for Netlify and configure custom headers

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// In your gatsby-config.js file
+
+export const plugins = [
+  {
+    resolve: `gatsby-plugin-netlify`,
+    options: {
+      headers: {
+        // Define custom headers for your single page
+        "/*": [
+          // Example custom header
+          "X-Frame-Options: DENY",
+          // Add more custom headers if needed
+        ],
+      },
+      allPageHeaders: [], // No need to add headers for all pages as there's only one page
+      mergeSecurityHeaders: true,
+      mergeCachingHeaders: true,
+      transformHeaders: (headers, path) => headers,
+      generateMatchPathRewrites: true,
+    },
+  },
+];
