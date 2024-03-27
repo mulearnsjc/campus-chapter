c9359cbb127967a0ec434084be102d2864247685
Alvin <alvindennis80@gmail.com>
2024-03-27 20:56:04 +0530

    Added mulearn loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 // import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
@@ -10,10 +11,26 @@ import ExploreLC from "./Components/ExploreLC/ExploreLC";
 import Team from "./Components/Team/Team";
 import Connect from "./Components/Connect/Connect";
 import Footer from "./Components/Footer/Footer";
+import MuLoader from "./Components/Home/Loader";
 function App() {
-
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+      setLoading (true);
+      setTimeout(() => {
+        setLoading(false);
+      },4000)
+  
+    }, []);
+  
     return (
-        <div className="appWrapper">
+        <div>
+            {
+      loading ? 
+      <div style={{minHeight:"100vh", width: "100%", display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <MuLoader />
+      </div>
+       :
+          <div>
             <Navbar />
             <Home />
             <About />
@@ -25,7 +42,9 @@ function App() {
             <Connect />
             <Footer />
         </div>
+            }
+        </div>
     );
-}
-
+        
+        }
 export default App;
